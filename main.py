1cb8a5428dd211c5394e28aab663d0d9f380a2c8
Matt Wilke <matt.wilke08@gmail.com>
2021-07-04 14:42:14 -0500

    initail commit

---
+++ main.py
@@ -0,0 +1,36 @@
+import turtle
+import pandas
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+image = "./blank_states_img.gif"
+
+data = pandas.read_csv("./50_states.csv")
+all_states = data.state.to_list()
+
+screen.addshape(image)
+turtle.shape(image)
+my_screen_writes = []
+correct_guesses = []
+
+while len(correct_guesses) < 50:
+
+    answer_state = screen.textinput(title=f"{len(correct_guesses)}/50 Guess a State", prompt="What is another state?").title()
+
+    if answer_state in all_states:
+        correct_guesses.append(answer_state)
+        new_state = turtle.Turtle()
+        new_state.penup()
+        new_state.hideturtle()
+        target_state = data[data.state == answer_state]
+        x_coord = int(target_state.x)
+        y_coord = int(target_state.y)
+        state_coord = (x_coord, y_coord)
+        new_state.goto(state_coord)
+        new_state.write(answer_state, font=("Arial", 8, "normal"))
+        my_screen_writes.append(new_state)
+
+
+turtle.mainloop()
+
+
